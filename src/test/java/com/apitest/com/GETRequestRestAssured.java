4275cfbef61b1f822b8e5109a737a80bab78eeb4
Ajinkya <ajkunjir786@gmail.com>
2022-08-03 20:49:09 -0400

    Added an example for POST and GET call

--- src/test/java/com/apitest/com/GETRequestRestAssured.java
+++ src/test/java/com/apitest/com/GETRequestRestAssured.java
@@ -1,6 +1,12 @@
 package com.apitest.com;
 
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import org.hamcrest.Matchers.*;
@@ -15,9 +21,53 @@ public class GETRequestRestAssured {
 		  given()
 		  .when()
 		  	.get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
-		  .then().
-		  		log()
-		  		.all();
+		  	.body();
+		
+	}
+	
+	@Test
+	public void anotherExample() {
+	  given()
+	  .when()
+	  	.get("https://official-joke-api.appspot.com/random_joke")
+	  	.body();
+	}
+	
+	@Test
+	public void GetBookDetails() {
+		
+		RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.request(Method.GET,"");
+		System.out.println("Status of the call received: " + response.getStatusCode());
+		System.out.println("The Response is: " + response.prettyPrint());
+		System.out.println("The Headers are: " + response.getHeaders());
+		
+	}
+	
+	@Test
+	public void DemoPOST() {
+		
+		final String body = "{\"name\":\"spidey\",\"job\":\"employee\"}";
+		RestAssured.baseURI = "https://reqres.in/api/users";
+	     
+		Response response = given()
+				.header("Content-Type", "application/json")
+				.and()
+				.body(body)
+				.when()
+				.post()
+				.then()
+				.extract().response();
+		
+		String pretty = response.asPrettyString();
+		System.out.println(pretty);
+		
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response1 = httpRequest.get();
+		System.out.println(response1.prettyPrint());
+
+	}
 	}
 
-}
+
